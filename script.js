b7c650370c62a9cd16d832691f2453270c5d0676
johncolobe <john_colobe@hotmail.com>
2026-01-09 17:51:42 -0500

    figured some css stuff out

--- script.js
+++ script.js
@@ -4,35 +4,67 @@ const resetButton = document.getElementById("resetButton");
 
 const cardLabel = document.getElementById("drawnCard");
 
-var card = "";
-var deck;
-var oldCards;
-var combatants;
+//holds current card
+let card;
+
+//stores current deck
+let deck = [];
+//holds previously drawn cards from current deck
+let oldCards = [];
+//list of characters that cards will be assigned to
+//NOT IMPLEMENTED YET
+let combatants = [];
+
+drawButton.onclick = function(){
+    //if loop?
+    //if no card, draw card. else add old card to list, then draw card
+    if (card) {
+       deck.oldCards.push(card);
+       listOldCards(card);
+    }
+    
+    card = deck.draw(deck.cards);
+
+    console.log(deck.oldCards);
+}
 
 shuffleButton.onclick = function(){
-    deck = deck.shuffle(deck.cards);
+    deck.shuffle(deck.cards);
     //console.log(deck.cards);
     cardLabel.textContent = "draw a card";
 }
 
-drawButton.onclick = function(){
-    card = deck.draw(deck.cards);
-}
-
 resetButton.onclick = function(){
     deck = new Deck();
+    card = null;
+    oldCards = [];
+
+    let list = document.getElementById("drawnCardsList");
+    while(list.hasChildNodes()){
+        list.removeChild(list.firstChild);
+    }
+
     cardLabel.textContent= "shuffle me";
     console.log("opened a fresh pack of cards");
     //console.log(deck.cards);
 }
 
+function listOldCards(cardText){
+    let list = document.getElementById("drawnCardsList");
+    let li = document.createElement("li")
+    li.textContent = cardText;
+    list.prepend(li);
+}
+
 class Deck {
     cards = [];
+    oldCards = [];
+    //reverse alphabetical order, for initiative
     suits = [
+        "Spades",
         "Hearts",
         "Diamonds",
         "Clubs",
-        "Spades"
     ];
 
     numbers = [2,3,4,5,6,7,8,9,10,"Jack","Queen","King","Ace"];
@@ -63,7 +95,7 @@ class Deck {
             [cards[i], cards[j]] = [cards[j], cards[i]];
         }
         console.log("shuffled the deck");
-        return deck;
+        return this;
     }
 
     draw (deck){
@@ -71,11 +103,18 @@ class Deck {
         console.log(`drew: ${card}`);
         //console.log(deck);
         cardLabel.textContent = card;
+        return card;
     }
+    
+}
 
     //take name and assign card for initiative tracker
     //to be implemented later
-    assign(cards){
+    function assign(cards){
         if (!deck.cards) return;
     }
-}
+
+    //tracker for odds of drawing a joker each time
+    function jokerOdds(){
+        //fancy math
+    }
